6e8dd42933dd684fcdb2606617ef4dffe535ca31
Salbrink <filip.salbrink@gmail.com>
2024-08-07 20:56:09 +0200

    Beginning of proper library

--- Functional Library/competition_selector.py
+++ Functional Library/competition_selector.py
@@ -98,7 +98,7 @@ def select_section(wait):
 
     print("\nSelect section:")
     for i, comp_sec in enumerate(competition_section_divs):
-
+        print(type(comp_sec))
         # Get and store the title string
         title_element = html_renderer.find_sub_element(comp_sec, title_element_selector, By.CSS_SELECTOR).text
         title_elements.append(title_element)
