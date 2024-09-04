d4113b4b44571dc345d743a742546cddd953c393
Luke <luke@zephyrtech.se>
2024-09-04 16:07:34 +0200

    Import TempusOpen using a from statement instead

--- luke_wip/main.py
+++ luke_wip/main.py
@@ -1,8 +1,7 @@
-
-import tempus
+from tempus import TempusOpen
 
 if __name__ == "__main__":
-    tempus_open = tempus.TempusOpen()
+    tempus_open = TempusOpen()
 
     pbs_filip = tempus_open.get_swimmer("Filip")
 
